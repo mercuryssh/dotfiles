bc7a1251a14d0d5d6fdaf42c2aee584e2268b813
Akiyama Joseph <joseph.estevez@protonmail.com>
2022-06-25 22:54:21 -0400

    feat: update `home-manager` config

--- flake.nix
+++ flake.nix
@@ -2,27 +2,38 @@
   description = "My NixOS Config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
+      username = "mash";
+      lib = nixpkgs.lib;
       pkgs = import nixpkgs {
         inherit system;
         config = { allowUnfree = true; }; # I Failed you rms
       };
-      lib = nixpkgs.lib;
     in {
+      homeConfigurations.${username} =
+        home-manager.lib.homeManagerConfiguration {
+          inherit system username;
+          stateVersion = "22.05";
+          homeDirectory = "/home/${username}";
+
+          configuration = { pkgs, config, ... }: {
+            programs.home-manager.enables = true;
+            programs.htop.enable = true;
+          };
+        };
       nixosConfigurations = {
         Hoshimachi = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [ ./hosts/Hoshimachi/configuration.nix ];
         };
       };
+
     };
 }
