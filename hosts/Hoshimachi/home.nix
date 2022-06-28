1ef852de123250f1fa41d223fbdaa9da54cc00d8
joseph Estevez <joseph.estevez@protonmail.com>
2022-06-28 14:08:31 -0400

    feat: update `home-manager` config

--- hosts/Hoshimachi/home.nix
+++ hosts/Hoshimachi/home.nix
@@ -1,16 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = [ pkgs.nodejs pkgs.nodePackages.pnpm ];
-
-  programs = {
-    htop.enable = true;
-    emacs.enable = true;
-
-    git = {
-      enable = true;
-      userName = "joseph Estevez";
-      userEmail = "joseph.estevez@protonmail.com";
-    };
-  };
+  imports = [ ../modules/programs];
 }
---
+++ modules/programs/default.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = with pkgs; [ nodejs pkgs.nodePackages.pnpm ];
+
+  programs = {
+    htop.enable = true;
+    emacs.enable = true;
+    vivaldi.enable = true;
+
+    git = {
+      enable = true;
+      userName = "joseph Estevez";
+      userEmail = "joseph.estevez@protonmail.com";
+    };
+  };
+
+}
