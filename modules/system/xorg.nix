9543f60d26714f340386f235d62a671ef9c51b8a
joseph Estevez <joseph.estevez@protonmail.com>
2022-07-23 02:58:36 -0400

    chore: exclude xterm pkg

--- modules/system/xorg.nix
+++ modules/system/xorg.nix
@@ -6,8 +6,10 @@
     libinput.enable = true;
     displayManager.gdm.enable = true;
     desktopManager = {
-      xterm.enable = false;
       gnome.enable = true;
     };
+    excludePackages = [
+      pkgs.xterm
+    ];
   };
 }
