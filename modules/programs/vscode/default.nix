7c58b1f018da0c9044f38a9235f37ad6bf42ba8f
joseph Estevez <joseph.estevez@protonmail.com>
2022-08-02 00:27:47 -0400

    remove vscode config

--- modules/programs/vscode/default.nix
+++ modules/programs/vscode/default.nix
@@ -14,91 +14,91 @@ in
       # package = pkgs.vscodium;
 
       # just the basics, the rest I load from the vscode sync the basic 
-      extensions = with pkgs.vscode-extensions; [
-        file-icons.file-icons
-        jnoortheen.nix-ide
-        vscodevim.vim
-      ];
+      # extensions = with pkgs.vscode-extensions; [
+      #   file-icons.file-icons
+      #   jnoortheen.nix-ide
+      #   vscodevim.vim
+      # ];
 
-      userSettings = {
-        # Disable Telemetry
-        "telemetry.telemetryLevel" = "off";
-        "redhat.telemetry.enabled" = false;
+      # userSettings = {
+      #   # Disable Telemetry
+      #   "telemetry.telemetryLevel" = "off";
+      #   "redhat.telemetry.enabled" = false;
 
-        # Git
-        "git.enableCommitSigning" = true;
+      #   # Git
+      #   "git.enableCommitSigning" = true;
 
-        # Text
-        "editor.tabSize" = 2;
-        "editor.fontFamily" = "system-ui";
-        "editor.fontSize" = 17;
-        "editor.fontLigatures" = true;
+      #   # Text
+      #   "editor.tabSize" = 2;
+      #   "editor.fontFamily" = "system-ui";
+      #   "editor.fontSize" = 17;
+      #   "editor.fontLigatures" = true;
 
-        # Layout
-        "window.dialogStyle" = "custom";
-        "window.titleBarStyle" = "custom";
-        "editor.minimap.enabled" = false;
-        "breadcrumbs.enabled" = false;
+      #   # Layout
+      #   "window.dialogStyle" = "custom";
+      #   "window.titleBarStyle" = "custom";
+      #   "editor.minimap.enabled" = false;
+      #   "breadcrumbs.enabled" = false;
 
-        # workbench
-        "workbench.editor.enablePreview" = false;
-        "workbench.colorTheme" = "GitHub Dark";
-        "workbench.iconTheme" = "file-icons";
-        "workbench.productIconTheme" = "icons-carbon";
-        "workbench.startupEditor" = "none";
-        "workbench.colorCustomizations" = {
-          "editor.lineHighlightBackground" = "#00000000";
-        };
+      #   # workbench
+      #   "workbench.editor.enablePreview" = false;
+      #   "workbench.colorTheme" = "GitHub Dark";
+      #   "workbench.iconTheme" = "file-icons";
+      #   "workbench.productIconTheme" = "icons-carbon";
+      #   "workbench.startupEditor" = "none";
+      #   "workbench.colorCustomizations" = {
+      #     "editor.lineHighlightBackground" = "#00000000";
+      #   };
 
-        # Languages preference
-        "javascript.preferences.quoteStyle" = "single";
-        "typescript.preferences.quoteStyle" = "single";
-        "typescript.tsdk" = "node_modules/typescript/lib";
-        "[typescript]" = {
-          "editor.defaultFormatter" = "esbenp.prettier-vscode";
-        };
-        "[javascript]" = {
-          "editor.defaultFormatter" = "esbenp.prettier-vscode";
-        };
-        "[typescriptreact]" = {
-          "editor.defaultFormatter" = "esbenp.prettier-vscode";
-        };
-        "[yaml]" = { "editor.defaultFormatter" = "redhat.vscode-yaml"; };
-        "[html]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
-        "[css]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
-        "[scss]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
-        "[json]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
-        "[jsonc]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
+      #   # Languages preference
+      #   "javascript.preferences.quoteStyle" = "single";
+      #   "typescript.preferences.quoteStyle" = "single";
+      #   "typescript.tsdk" = "node_modules/typescript/lib";
+      #   "[typescript]" = {
+      #     "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      #   };
+      #   "[javascript]" = {
+      #     "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      #   };
+      #   "[typescriptreact]" = {
+      #     "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      #   };
+      #   "[yaml]" = { "editor.defaultFormatter" = "redhat.vscode-yaml"; };
+      #   "[html]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
+      #   "[css]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
+      #   "[scss]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
+      #   "[json]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
+      #   "[jsonc]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
 
-        # excludes Files
-        "files.exclude" = {
-          "**/node_modules" = true;
-          "**/dist" = true;
-          "**/.next" = true;
-        };
+      #   # excludes Files
+      #   "files.exclude" = {
+      #     "**/node_modules" = true;
+      #     "**/dist" = true;
+      #     "**/.next" = true;
+      #   };
 
-        # Updates
-        "update.mode" = "manual";
+      #   # Updates
+      #   "update.mode" = "manual";
 
-        /* Background Image */
-        "background.enabled" = true;
-        "background.loop" = false;
-        "background.useDefault" = false;
-        "background.useFront" = false;
-        "background.customImages" = [
-          "/home/mash/.config/Code/Keqing_1.webp"
-        ];
-        "background.style" = {
-          "height" = "100%";
-          "z-index" = "4444";
-          "margin-left" = "60%";
-          "margin-top" = "10%";
-          "background-position" = "absolute";
-          "background-repeat" = "no-repeat";
-          "background-size" = "200px";
-          "opacity" = 0.8;
-        };
-      };
+      #   /* Background Image */
+      #   "background.enabled" = true;
+      #   "background.loop" = false;
+      #   "background.useDefault" = false;
+      #   "background.useFront" = false;
+      #   "background.customImages" = [
+      #     "/home/mash/.config/Code/Keqing_1.webp"
+      #   ];
+      #   "background.style" = {
+      #     "height" = "100%";
+      #     "z-index" = "4444";
+      #     "margin-left" = "60%";
+      #     "margin-top" = "10%";
+      #     "background-position" = "absolute";
+      #     "background-repeat" = "no-repeat";
+      #     "background-size" = "200px";
+      #     "opacity" = 0.8;
+      #   };
+      # };
     };
   };
 }
