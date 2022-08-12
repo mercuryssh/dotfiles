0c9c0fe18dce797438eb0799d3ab4a55d0f36d74
joseph Estevez <joseph.estevez@protonmail.com>
2022-08-12 17:35:45 -0400

    feat: override vscode version

--- modules/programs/vscode/default.nix
+++ modules/programs/vscode/default.nix
@@ -1,104 +1,24 @@
 { config, pkgs, lib, ... }:
 
 with lib;
-let cfg = config.modules.programs.vscode;
+let
+  cfg = config.modules.programs.vscode;
+  vscode = (pkgs.vscode.override { isInsiders = false; }).overrideAttrs (oldAttrs: rec {
+    src = (builtins.fetchTarball {
+      url = "https://update.code.visualstudio.com/1.70.1/linux-x64/stable";
+      sha256 = "0fay23spha42rhbdd69bpl6h22vqg3aj7hhnld6gxfhv8z1scnsz";
+    });
+  });
 in
 {
   options.modules.programs.vscode = { enable = mkEnableOption false; };
 
   config = mkIf cfg.enable {
-    home.file.".config/Code/Keqing_1.webp".source = ../../../assets/icons/Keqing_1.webp;
-
     programs.vscode = {
       enable = true;
-      # package = pkgs.vscodium;
-
-      # just the basics, the rest I load from the vscode sync the basic 
-      # extensions = with pkgs.vscode-extensions; [
-      #   file-icons.file-icons
-      #   jnoortheen.nix-ide
-      #   vscodevim.vim
-      # ];
-
-      # userSettings = {
-      #   # Disable Telemetry
-      #   "telemetry.telemetryLevel" = "off";
-      #   "redhat.telemetry.enabled" = false;
-
-      #   # Git
-      #   "git.enableCommitSigning" = true;
-
-      #   # Text
-      #   "editor.tabSize" = 2;
-      #   "editor.fontFamily" = "system-ui";
-      #   "editor.fontSize" = 17;
-      #   "editor.fontLigatures" = true;
-
-      #   # Layout
-      #   "window.dialogStyle" = "custom";
-      #   "window.titleBarStyle" = "custom";
-      #   "editor.minimap.enabled" = false;
-      #   "breadcrumbs.enabled" = false;
-
-      #   # workbench
-      #   "workbench.editor.enablePreview" = false;
-      #   "workbench.colorTheme" = "GitHub Dark";
-      #   "workbench.iconTheme" = "file-icons";
-      #   "workbench.productIconTheme" = "icons-carbon";
-      #   "workbench.startupEditor" = "none";
-      #   "workbench.colorCustomizations" = {
-      #     "editor.lineHighlightBackground" = "#00000000";
-      #   };
-
-      #   # Languages preference
-      #   "javascript.preferences.quoteStyle" = "single";
-      #   "typescript.preferences.quoteStyle" = "single";
-      #   "typescript.tsdk" = "node_modules/typescript/lib";
-      #   "[typescript]" = {
-      #     "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      #   };
-      #   "[javascript]" = {
-      #     "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      #   };
-      #   "[typescriptreact]" = {
-      #     "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      #   };
-      #   "[yaml]" = { "editor.defaultFormatter" = "redhat.vscode-yaml"; };
-      #   "[html]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
-      #   "[css]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
-      #   "[scss]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
-      #   "[json]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
-      #   "[jsonc]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; };
-
-      #   # excludes Files
-      #   "files.exclude" = {
-      #     "**/node_modules" = true;
-      #     "**/dist" = true;
-      #     "**/.next" = true;
-      #   };
-
-      #   # Updates
-      #   "update.mode" = "manual";
-
-      #   /* Background Image */
-      #   "background.enabled" = true;
-      #   "background.loop" = false;
-      #   "background.useDefault" = false;
-      #   "background.useFront" = false;
-      #   "background.customImages" = [
-      #     "/home/mash/.config/Code/Keqing_1.webp"
-      #   ];
-      #   "background.style" = {
-      #     "height" = "100%";
-      #     "z-index" = "4444";
-      #     "margin-left" = "60%";
-      #     "margin-top" = "10%";
-      #     "background-position" = "absolute";
-      #     "background-repeat" = "no-repeat";
-      #     "background-size" = "200px";
-      #     "opacity" = 0.8;
-      #   };
-      # };
+      package = vscode;
     };
+
+    home.file.".config/Code/Keqing_1.webp".source = ../../../assets/icons/Keqing_1.webp;
   };
 }
